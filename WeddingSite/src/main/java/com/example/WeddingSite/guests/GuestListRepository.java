51c77c44c7d5845cc6c657754d25aaec7c4f1291
eliot390 <eliot390@github.com>
2025-02-25 14:35:34 -0800

    cleaned up guest service and repo

--- WeddingSite/src/main/java/com/example/WeddingSite/guests/GuestListRepository.java
+++ WeddingSite/src/main/java/com/example/WeddingSite/guests/GuestListRepository.java
@@ -2,6 +2,7 @@ package com.example.WeddingSite.guests;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,9 +10,9 @@ import java.util.Optional;
 
 @Repository
 public interface GuestListRepository extends JpaRepository<GuestList, Long> {
-  @Query("SELECT g FROM GuestList g WHERE g.full_name = ?1")
-  Optional<GuestList> findGuestByName(String full_name);
+  @Query("SELECT g FROM GuestList g WHERE g.full_name = :full_name")
+  Optional<GuestList> findGuestByName(@Param("full_name") String full_name);
 
-  @Query("SELECT g FROM GuestList g WHERE g.group_pairing = ?1")
-  List<GuestList> findGuestByPairing(Integer group_pairing);
+  @Query("SELECT g FROM GuestList g WHERE g.group_pairing = :group_pairing")
+  List<GuestList> findGuestByPairing(@Param("group_pairing") Integer group_pairing);
 }
--- WeddingSite/src/main/java/com/example/WeddingSite/guests/GuestListService.java
+++ WeddingSite/src/main/java/com/example/WeddingSite/guests/GuestListService.java
@@ -1,10 +1,7 @@
 package com.example.WeddingSite.guests;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
